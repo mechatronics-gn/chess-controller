5eb075424334b016449aa7d83eea117fc04786f6
Janghyub Seo <jhyub06@gmail.com>
2023-10-14 11:21:35 +0900

    use Coord in lcd, format code

--- LCD.cpp
+++ LCD.cpp
@@ -11,37 +11,32 @@ LCD::LCD(uint8_t addr) {
 
     this->state = LCD_STATE_HELLO;
 
-    char *tmp = malloc(sizeof(char));
-    tmp[0] = "\0";
+    char *tmp = (char *)malloc(sizeof(char));
+    tmp[0] = '\0';
     this->algebraic_notation = tmp;
     this->algebraic_notation_lock = false;
 }
 
-void LCD::set_state(uint8_t state) {
-    this->state = state;
-}
+void LCD::set_state(uint8_t state) { this->state = state; }
 
-void LCD::set_edit_value(uint8_t x, uint8_t y) {
+void LCD::set_edit_value(struct Coord coord) {
     if (!this->state) return;
 
     switch (this->state) {
         case LCD_EDIT_TARGET_FROM:
-            this->from_x = x;
-            this->from_y = y;
+            this->from = coord;
             break;
         case LCD_EDIT_TARGET_TO:
-            this->to_x = x;
-            this->to_y = y;
+            this->to = coord;
             break;
     }
 }
 
-void LCD::set_edit_target(uint8_t target) {
-    this->edit_target = target;
-}
+void LCD::set_edit_target(uint8_t target) { this->edit_target = target; }
 
 void LCD::set_move_algebraic_notation(char *notation) {
-    while (this->algebraic_notation_lock) {}
+    while (this->algebraic_notation_lock) {
+    }
     this->algebraic_notation_lock = true;
     char *tmp = this->algebraic_notation;
     this->algebraic_notation = notation;
@@ -49,14 +44,13 @@ void LCD::set_move_algebraic_notation(char *notation) {
     this->algebraic_notation_lock = false;
 }
 
-void LCD::update() { // 미래의 나 화이팅.
-    
+void LCD::update() {  // 미래의 나 화이팅.
 }
 
-char *coord_display(uint8_t x, uint8_t y) {
-    char *ret = malloc(3 * sizeof(char));
-    ret[0] = (char)x + 97; // 0 -> 'a'
-    ret[1] = (char)x + 49; // 0 -> '1'
+char *coord_display(struct Coord coord) {
+    char *ret = (char *)malloc(3 * sizeof(char));
+    ret[0] = (char)coord.x + 97;  // 0 -> 'a'
+    ret[1] = (char)coord.y + 49;  // 0 -> '1'
     ret[2] = '\0';
 
     return ret;
--- LCD.h
+++ LCD.h
@@ -3,49 +3,61 @@
 
 #include <LiquidCrystal_I2C.h>
 
+#include "Coord.h"
+
 #define LCD_COLS (2)
 #define LCD_ROWS (16)
 
-#define LCD_STATE_HELLO (0) /* This displays a simple hello message */
-#define LCD_STATE_STANDBY (1) /* This shows the timer, which is not passing, before starting the game */
-#define LCD_STATE_MOVING_CPU (2) /* This waits until cpu move by motor is done */
-#define LCD_STATE_MOVING_PLAYER (3) /* This waits until player move by motor is done */
-#define LCD_STATE_WAIT_PLAYER (4) /* This waits player input */
-#define LCD_STATE_VICTORY (5) /* This is player victory */
-#define LCD_STATE_GAMEOVER (6) /* This is cpu victory */
+/* This displays a simple hello message */
+#define LCD_STATE_HELLO (0)
+/* This shows the timer, which is not passing, before starting the game */
+#define LCD_STATE_STANDBY (1)
+/* This waits until cpu move by motor is done */
+#define LCD_STATE_MOVING_CPU (2)
+/* This waits until player move by motor is done */
+#define LCD_STATE_MOVING_PLAYER (3)
+/* This waits player input */
+#define LCD_STATE_WAIT_PLAYER (4)
+/* This is player victory */
+#define LCD_STATE_VICTORY (5)
+/* This is cpu victory */
+#define LCD_STATE_GAMEOVER (6)
 
 #define LCD_EDIT_TARGET_NONE (0)
 #define LCD_EDIT_TARGET_FROM (1)
 #define LCD_EDIT_TARGET_TO (2)
 
 class LCD {
-    public:
-        LCD(uint8_t addr);
+   public:
+    LCD(uint8_t addr);
 
-        void set_state(uint8_t state);
+    void set_state(uint8_t state);
 
-        void set_edit_value(uint8_t x, uint8_t y);
-        void set_edit_target(uint8_t target);
+    /**
+     * Set the edit target of which we are aiming to change coord value.
+     */
+    void set_edit_target(uint8_t target);
+    void set_edit_value(struct Coord coord);
 
-        void set_move_algebraic_notation(char *notation);
+    void set_move_algebraic_notation(char *notation);
 
-        void update();
-    private:
-        uint8_t addr;
-        LiquidCrystal_I2C *lc;
+    void update();
 
-        uint8_t state;
+   private:
+    uint8_t addr;
+    LiquidCrystal_I2C *lc;
 
-        uint8_t from_x;
-        uint8_t from_y;
-        uint8_t to_x;
-        uint8_t to_y;
-        uint8_t edit_target;
+    uint8_t state;
 
-        char *algebraic_notation;
-        bool algebraic_notation_lock;
+    struct Coord from;
+    struct Coord to;
+
+    uint8_t edit_target;
+
+    char *algebraic_notation;
+    bool algebraic_notation_lock;
 };
 
-char *coord_display(uint8_t x, uint8_t y);
+char *coord_display(struct Coord coord);
 
 #endif
